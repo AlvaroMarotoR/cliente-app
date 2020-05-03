5332f67b906cbc886b034d66f67820ca2d18387f
AlvaroMarotoR <maroto00ecf@hotmail.com>
2020-05-03 02:38:19 +0200

    Unión con Spring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { ClientesComponent } from './clientes/clientes.component';
 import { ClienteService } from './clientes/cliente.service'
 import { RouterModule, Routes} from '@angular/router'
 
+import {HttpClientModule} from '@angular/common/http'
+
+
 
 const routes: Routes = [
   {path: '', redirectTo: '/clientes', pathMatch:'full'}, //Path vacio que redirige a clientes
@@ -27,6 +30,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot(routes) //Le pasamos la variable del array de direcciones
   ],
   providers: [ClienteService],
--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import {map} from 'rxjs/operators';
 
 //logica de NEGOCIO, MODELO
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ClienteService {
+private urlEndPoint:string = 'http://localhost:8080/api/clientes';
+  constructor( private http: HttpClient) { }
 
-  constructor() { }
+  getClientes(): Observable<Cliente[]> {
+    //return CLIENTES;
+    return this.http.get(this.urlEndPoint).pipe(
+      map( (response) => response as Cliente[] )
+    );
 
-  getClientes(): Cliente[] {
-    return CLIENTES;
   }
 }
